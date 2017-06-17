f1f5fab21721e587bb1a218290639ac53f129477
littleflute <13699175164@sina.cn>
2017-06-17 17:12:00 -0500

    Add files via upload

---
+++ Shakedown Street [Bonus Tracks]/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Good Lovin'.mp3";		
+f[2]	= "02 France.mp3";		
+f[3]	= "03 Shakedown Street.mp3";		
+f[4]	= "04 Serengetti.mp3";		
+f[5]	= "05 Fire on the Mountain.mp3";		
+f[6]	= "06 I Need a Miracle.mp3";		
+f[7]	= "07 From the Heart of Me.mp3";		
+f[8]	= "08 Stagger Lee.mp3";		
+f[9]	= "09 All New Minglewood Blues.mp3";		
+f[10]	= "10 If I Had the World to Give.mp3";		
+f[11]	= "11 Good Lovin' [-][Studio Outtake].mp3";		
+f[12]	= "12 Ollin Arageed [-][Live].mp3";		
+f[13]	= "13 Fire on the Mountain [-][Live].mp3";		
+f[14]	= "14 Stagger Lee [-][Live].mp3";		
+f[15]	= "15 All New Minglewood Blues [-][Live].mp3";		
